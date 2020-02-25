ebd4bb6fa6bcd7166c22fd8aa02fc824e9078eab
Matheus H. de Souza <mh.matheussouza@gmail.com>
2020-02-25 19:40:47 -0300

    Aula sobre Object.getOwnPropertyDescriptors()

---
+++ property-descriptor.js
@@ -0,0 +1,9 @@
+const PlayStation = {
+    marca: 'Sony',
+    controles: 2,
+    jogar() {
+        console.log('insira um jogo');
+    }
+};
+
+console.log(Object.getOwnPropertyDescriptors(PlayStation));
