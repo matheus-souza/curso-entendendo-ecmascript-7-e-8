e0588cbbcd8cb70249b41637f46c4760455a7573
Matheus H. de Souza <mh.matheussouza@gmail.com>
2020-02-25 19:41:03 -0300

    Aula sobre virgulas sobrando

---
+++ virgulas.js
@@ -0,0 +1,14 @@
+function imprimeNoConsole(param1, param2,) {
+    return `${param1} e ${param2}`;
+}
+
+console.log(imprimeNoConsole('ES7', 'ES8',));
+
+const Curso = {
+    titulo: 'ECMAScript 7 e 8',
+    plataforma: 'Udemy',
+};
+
+const nomes = ['Mario', 'Luiza', 'João',];
+console.log(Curso);
+console.log(nomes);
